d3aa3b3a5debdc69019ce4caac0653e988d264a0
Midhilesh4890 <chintu.midhilesh@gmail.com>
2021-01-30 17:08:02 +0530

    python package for text preprocessing

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python Text Preprocessing for NLP
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'textpreprocess_nlp', #this should be unique
+	include_package_data=True,
+	version = '0.10.3',
+	author = 'Midhilesh Momidi',
+	author_email = 'chintu.midhilesh@gmail.com',
+	description = 'This is text preprocessing package for NLP',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
